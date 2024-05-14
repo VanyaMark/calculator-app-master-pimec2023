9722e96e41c349a832d6f2fce0227e8ed177a0ce
VanyaMark <markova.vanya@gmail.com>
2024-05-14 22:18:07 +0200

    working on second operator case

--- index.js
+++ index.js
@@ -1,76 +1,87 @@
 // Buena suerte!
 //Extract from DOM number buttons and operators in arrays
-let numberButtons = document.querySelectorAll('[data-num]');
-let mathOperators = document.querySelectorAll('[data-operator]');
+let numberButtons = document.querySelectorAll("[data-num]");
+let mathOperators = document.querySelectorAll("[data-operator]");
 
 //Extract from DOM other buttons in variables
-let delButton = document.querySelector('[data-delete]');
-let resetButton = document.querySelector('[data-reset]');
-let outputButton = document.querySelector('[data-output]');
+let delButton = document.querySelector("[data-delete]");
+let resetButton = document.querySelector("[data-reset]");
+let outputButton = document.querySelector("[data-output]");
 
 //Extract from DOM div elements where one should showcase the numbers and the results
-let currentOperand = document.querySelector('.current-operand');
-let previousOperand = document.querySelector('.previous-operand');
+let currentOperand = document.querySelector(".current-operand");
+let previousOperand = document.querySelector(".previous-operand");
 
 //State value - number array where to collect the pressed digits
 let firstNumberValue = [];
 let secondNumberValue = [];
-let mathOperator = '';
-let firstOperator = false;
+let mathOperator = "";
 
-let mathOperatorsArr = [...mathOperators].map(mathOperatorButton => mathOperatorButton.dataset.operator);
+
+let mathOperatorsArr = [...mathOperators].map(
+  (mathOperatorButton) => mathOperatorButton.dataset.operator
+);
 
 //help functions
 const mathOperation = (firstNumber, secondNumber, operator) => {
-    let firstNumberToNumber = (parseFloat(firstNumber.join('')));
-    let secondNumberToNumber = (parseFloat(secondNumber.join('')));
+  let firstNumberToNumber = parseFloat(firstNumber.join(""));
+  let secondNumberToNumber = parseFloat(secondNumber.join(""));
 
-    let text = `${firstNumberToNumber} ${mathOperator} ${secondNumberToNumber}`;
-    console.log('text ', text)
- let result = eval(text);
- console.log('eval', result);
+  let text = `${firstNumberToNumber} ${mathOperator} ${secondNumberToNumber}`;
+  console.log("text ", text);
+  let result = eval(text);
+  console.log("eval", result);
 
- return result;
-
-}
-
-//Declare functions handling event listeners
-const numberButtonClick = num => {
-    let checkOperator = mathOperatorsArr.includes(mathOperator);
-    console.log(checkOperator)
-    if (checkOperator) {
-        currentOperand.textContent = '';
-        secondNumberValue.push(num);
-        currentOperand.textContent = secondNumberValue.join('');
-    }
-    else {
-        firstNumberValue.push(num);
-        currentOperand.textContent = firstNumberValue.join('');
-    }
+  return result;
 };
 
-const mathOperatorClick = operator => {
-    mathOperator = operator;
-    firstOperator = true;
-    let firstNumberAndOperator = firstNumberValue.concat(operator)
-    previousOperand.textContent = firstNumberAndOperator.join('');
-    currentOperand.textContent = '';
+//Declare functions handling event listeners
+const numberButtonClick = (num) => {
+  let checkOperator = mathOperatorsArr.includes(mathOperator);
+  console.log(checkOperator);
+  if (checkOperator) {
+    currentOperand.textContent = "";
+    secondNumberValue.push(num);
+    currentOperand.textContent = secondNumberValue.join("");
+  } else {
+    firstNumberValue.push(num);
+    currentOperand.textContent = firstNumberValue.join("");
+  }
+};
+
+const mathOperatorClick = (operator) => {
+  if (secondNumberValue.length > 0) {
+    console.log(mathOperator)
+    showResult()
+  }
+  mathOperator = operator;
+  let firstNumberAndOperator = firstNumberValue.concat(operator);
+  previousOperand.textContent = firstNumberAndOperator.join("");
+  currentOperand.textContent = "";
 };
 
 const showResult = () => {
-    let finalResult = mathOperation(firstNumberValue, secondNumberValue, mathOperator)
-    console.log('showResult: ', finalResult);
-    currentOperand.textContent = finalResult;
-    previousOperand.textContent = '';
+  let finalResult = mathOperation(
+    firstNumberValue,
+    secondNumberValue,
+    mathOperator
+  );
+  console.log("showResult: ", finalResult);
+  currentOperand.textContent = finalResult;
+  previousOperand.textContent = "";
 };
 
 //Add click event listener to all number buttons
 numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', (event) => numberButtonClick(event.target.dataset.num))
+  numberButton.addEventListener("click", (event) =>
+    numberButtonClick(event.target.dataset.num)
+  );
 });
 
 mathOperators.forEach((mathOperator) => {
-    mathOperator.addEventListener('click', (event) => mathOperatorClick(event.target.dataset.operator))
+  mathOperator.addEventListener("click", (event) =>
+    mathOperatorClick(event.target.dataset.operator)
+  );
 });
 
-outputButton.addEventListener('click', showResult);
+outputButton.addEventListener("click", showResult);
