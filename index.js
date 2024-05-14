d2c8ee67b9150b832e3d6c5b990568d666fd850a
VanyaMark <markova.vanya@gmail.com>
2024-05-14 14:20:26 +0200

    extracting elements from DOM into js arrays and variables

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Buena suerte!
+//Extract from DOM number buttons and operators in arrays
+let numberButtons = document.querySelectorAll('[data-num]');
+let mathOperators = document.querySelectorAll('[data-opearot]');
+
+//Extract from DOM other buttons in variables
+let delButton = document.querySelector('[data-delete]');
+let resetButton = document.querySelector('[data-reset]');
+let outputButton = document.querySelector('[data-output]');
